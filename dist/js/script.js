e150150860b9e01b52c319f4ee4c25391df2768f
mutairiahmed <goldencode@outlook.sa>
2023-11-17 19:44:10 +0300

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,7 @@
+const MobileBtn = document.querySelector("#mobile-btn")
+const MobileMenu = document.querySelector("#mobile-menu")
+
+MobileBtn.addEventListener('click',() => {
+
+    MobileMenu.classList.toggle('hidden');
+});
